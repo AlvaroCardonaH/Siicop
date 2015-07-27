7d18d9e0f2dd667022a0f9962dfd1654e5b47de4
AlvaroCardonaH <alvarocardona91@gmail.com>
2015-07-27 15:51:50 -0500

    Prueba con ImportCsv
    Se detecto que no funcionaba por la version de Yii

--- upload/reporte.php
+++ upload/reporte.php
@@ -4,11 +4,11 @@
                     "delimiter"=>";",
 		    "textDelimiter"=>"\"",
                     "mode"=>1,
-                    "perRequest"=>5000,
+                    "perRequest"=>10,
                     "csvKey"=>"",
                     "tableKey"=>"",
                     "columns"=>array(
-                        "IdReportePedidosxPdvDetalle"=>"", "IdReportePedidosxPdv"=>1, "NumeroDeRegistro"=>2, "CodigoPdv"=>3, "NombrePdv"=>4, "Caja"=>5, "Factura"=>6, "Valor"=>7, "NombreMensajero"=>8, "CelularMensajero"=>9, "NombreCliente"=>10, "DireccionCliente"=>11, "TelefonoCliente"=>12, "EstadoPedido"=>13, "CausalEntrega"=>14, "FechaPos"=>15, "FechaAceptacion"=>16, "TiempoEspera"=>17, "FechaEntrega"=>18, "TiempoTotalMinutos"=>19, "IdRuta"=>20, "CodigoEstadoRegistro"=>21, "EsDiaFestivo"=>22, 
+                        "IdReportePedidosxPdvDetalle"=>"", "IdReportePedidosxPdv"=>1, "CodigoPdv"=>2, "NombrePdv"=>3, "FechaPos"=>4, "TiempoTotalMinutos"=>5, 
                     ),
                 );
             ?>
